61dcafac44d4dd01bb1e5a141c46cef452db6664
Ubuntu <ubuntu@ip-172-31-16-159.us-east-2.compute.internal>
2026-02-13 19:09:50 +0000

    deploy: HashKey testnet (chainId 133)
    Contracts:
    - AGNTToken: 0x7379C9d687F8c22d41be43fE510F8225afF253f6
    - AgentRegistry: 0xb044E947E8eCf2d954E9C1e26970bEe128e9EB49
    - TaskMarketplace: 0x7907ec09f1d1854Fd4dA26E1a9e357Fd0d797061
    
    RPC: https://testnet.hsk.xyz

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -18,6 +21,11 @@ const config: HardhatUserConfig = {
       url: process.env.HASHKEY_RPC_URL || "https://hashkeychain-testnet.alt.technology",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 133
+    },
+    hashkeyMainnet: {
+      url: "https://mainnet.hsk.xyz",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 177
     }
   }
 };
